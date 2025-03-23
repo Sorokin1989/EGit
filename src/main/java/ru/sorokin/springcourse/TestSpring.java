b158acb141a5720c36901d2a7f184c047ed838ed
Dmitrii <sorokindm1try@yandex.ru>
2025-03-23 11:37:08 +0300

    newSpringApp 2.1

--- src/main/java/ru/sorokin/springcourse/TestSpring.java
+++ src/main/java/ru/sorokin/springcourse/TestSpring.java
@@ -8,8 +8,18 @@ public class TestSpring {
         //Music music= context.getBean("musicBean",Music.class);
         //MusicPlayer musicPlayer=new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusicList();
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comparison=firstMusicPlayer==secondMusicPlayer;
+        System.out.println(comparison);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(20);
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+
 
         //System.out.println(musicPlayer.getName());
         //System.out.println(musicPlayer.getVolume());
