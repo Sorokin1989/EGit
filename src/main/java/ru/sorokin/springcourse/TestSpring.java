6ad3eab5d4baedb661e496f14961029dc418c5b2
Dmitrii <sorokindm1try@yandex.ru>
2025-03-24 13:44:32 +0300

    newSpringApp 2.7

--- src/main/java/ru/sorokin/springcourse/TestSpring.java
+++ src/main/java/ru/sorokin/springcourse/TestSpring.java
@@ -5,13 +5,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-      Music music = context.getBean("classicalMusic", ClassicalMusic.class);
-      Music music1=context.getBean("jazzMusic", JazzMusic.class);
+       // Music music = context.getBean("classicalMusic", ClassicalMusic.class);
+        //Music music1 = context.getBean("jazzMusic", JazzMusic.class);
 
-       MusicPlayer musicPlayer=new MusicPlayer(music);
-       MusicPlayer musicPlayer1=new MusicPlayer(music1);
-       musicPlayer1.playMusic();
-       musicPlayer.playMusic();
+       // MusicPlayer musicPlayer = new MusicPlayer(music);
+        //MusicPlayer musicPlayer1 = new MusicPlayer(music1);
+        //musicPlayer1.playMusic();
+        //musicPlayer.playMusic();
+
+        MusicPlayer musicPlayer=context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
         context.close();
     }
 }
