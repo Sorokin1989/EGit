2f86a863f0edfe68cb5b30d3ddf30a14136ab0e1
Dmitrii <sorokindm1try@yandex.ru>
2025-03-24 12:44:14 +0300

    newSpringApp 2.6

--- src/main/java/ru/sorokin/springcourse/MusicPlayer.java
+++ src/main/java/ru/sorokin/springcourse/MusicPlayer.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MusicPlayer {
-    //private Music music;
-    private List<Music> musicList = new ArrayList<>();
+    private Music music;
+    //private List<Music> musicList = new ArrayList<>();
 
 
     private String name;
     private int volume;
 
-    public void setMusicList(List<Music> musicList) {
-        this.musicList = musicList;
-    }
-
+    //public void setMusicList(List<Music> musicList) {
+    //    this.musicList = musicList;
+    //}
+//
     public String getName() {
         return name;
     }
@@ -30,21 +30,21 @@ public class MusicPlayer {
     public void setVolume(int volume) {
         this.volume = volume;
     }
-    // public MusicPlayer(Music music) {
-    //  this.music=music;
-    // }
-    // public MusicPlayer() {
-
-    //  }
-
-    public void setMusic(Music music) {
-        this.musicList = musicList;
+    public MusicPlayer(Music music) {
+     this.music=music;
     }
+    public MusicPlayer() {
 
-    public void playMusicList() {
-        for (Music music : musicList) {
-            System.out.println("Playing: " + music.getSong());
-        }
+     }
+
+   //public void setMusic(Music music) {
+   //    this.musicList = musicList;
+   //}
+
+   public void playMusic() {
+   //    for (Music music : musicList) {
+         System.out.println("Playing: " + music.getSong());
+   //    }
 
     }
 }
--- src/main/java/ru/sorokin/springcourse/TestSpring.java
+++ src/main/java/ru/sorokin/springcourse/TestSpring.java
@@ -5,34 +5,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
-        System.out.println(classicalMusic.getSong());
+      Music music = context.getBean("classicalMusic", ClassicalMusic.class);
+      Music music1=context.getBean("jazzMusic", JazzMusic.class);
 
-        ClassicalMusic classicalMusic2 = context.getBean("musicBean", ClassicalMusic.class);
-        ClassicalMusic classicalMusic3 = context.getBean("musicBean", ClassicalMusic.class);
-        ClassicalMusic classicalMusic4 = context.getBean("musicBean", ClassicalMusic.class);
-        ClassicalMusic classicalMusic5 = context.getBean("musicBean", ClassicalMusic.class);
-
-
-
-        //Music music= context.getBean("musicBean",Music.class);
-        //MusicPlayer musicPlayer=new MusicPlayer(music);
-
-        // MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        // MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//
-        // boolean comparison=firstMusicPlayer==secondMusicPlayer;
-        // System.out.println(comparison);
-        // System.out.println(firstMusicPlayer);
-        // System.out.println(secondMusicPlayer);
-//
-        // firstMusicPlayer.setVolume(20);
-        // System.out.println(firstMusicPlayer.getVolume());
-        // System.out.println(secondMusicPlayer.getVolume());
-//
-
-        //System.out.println(musicPlayer.getName());
-        //System.out.println(musicPlayer.getVolume());
+       MusicPlayer musicPlayer=new MusicPlayer(music);
+       MusicPlayer musicPlayer1=new MusicPlayer(music1);
+       musicPlayer1.playMusic();
+       musicPlayer.playMusic();
         context.close();
     }
 }
