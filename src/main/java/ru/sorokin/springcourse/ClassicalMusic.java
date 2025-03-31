c415964caee17d839a90e148b741f791f0bdbebb
Dmitrii <sorokindm1try@yandex.ru>
2025-03-31 14:41:31 +0300

    newSpringApp 3.5

--- src/main/java/ru/sorokin/springcourse/ClassicalMusic.java
+++ src/main/java/ru/sorokin/springcourse/ClassicalMusic.java
@@ -1,11 +1,13 @@
 package ru.sorokin.springcourse;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Scope("singleton")
 public class ClassicalMusic implements Music {
     private List<String> songs = new ArrayList<String>();
 
